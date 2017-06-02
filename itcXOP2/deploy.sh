54cbcf5c06123d75c0fc0727bf4f82201d92e37f
Thomas Braun <thomas.braun@byte-physics.de>
2017-06-02 11:47:27 +0200

    ITC XOP2: Fix deploy script PDB paths
    Broken since the initial implementation in c6eb2279 (ITC XOP2: Add deploy
    script, 2017-04-06).

--- itcXOP2/deploy.sh
+++ itcXOP2/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 cp VC2015/itcXOP2.xop    ../../../XOPs-IP7/
-cp VC2015/itcXOP2.pdb    PDBs
+cp VC2015/Release/itcXOP2.pdb    PDBs
 
 cp VC2015/itcXOP2-64.xop ../../../XOPs-IP7-64bit/
-cp VC2015/itcXOP2-64.pdb PDBs
+cp VC2015/x64/Release/itcXOP2-64.pdb PDBs
