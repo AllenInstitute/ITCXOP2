4fa9329b93fa056341aa61128d520cc187d672e9
Thomas Braun <thomas.braun@byte-physics.de>
2021-12-02 18:00:44 +0100

    ITCOpenDevice2: Make it more robust on errors
    We have one report from a user (Tim Jarsky) that sometimes the device
    open/close state of the XOP is not the same as the one from the ITC
    library.
    
    This can for example happen if the ITCDLL::ITC_InitDevice method throws.
    We now remove the device again in this case from our internal list.
    
    And we also postpone the return of the opened device ID after having
    done all initialization.

--- src/ITCOpenDevice2.cpp
+++ src/ITCOpenDevice2.cpp
@@ -1,4 +1,5 @@
 #include "ITC_StandardHeaders.h"
+#include "HelperFunctions_ITC.h"
 #include "DeviceIDClass.h"
 
 // This file is part of the `ITCXOP2` project and licensed under BSD-3-Clause.
@@ -42,27 +43,35 @@ extern "C" int ExecuteITCOpenDevice2(ITCOpenDevice2RuntimeParamsPtr p)
   DeviceIDHelper DeviceID(currentDeviceID);
   DeviceIDHelper::setCurrentThreadDeviceID(currentDeviceID);
 
+  try
+  {
+    // Do ITC_GlobalConfig
+    //
+    // NOTE: Igor will provide the XOP with pointers to individual FIFO.
+    // The internal FIFOs are not used.
+    // Disable the internal FIFOs using ITC_GlobalConfig to save memory
+    ITCGlobalConfig currGlobalConfig;
+    ZeroMemory(&currGlobalConfig, sizeof(currGlobalConfig));
+    currGlobalConfig.SoftwareFIFOSize = 0;
+    ITCDLL::ITC_GlobalConfig(&currGlobalConfig);
+
+    // Initialize the device
+    ITCDLL::ITC_InitDevice(DeviceID, NULL);
+
+    const long key = (InstruKey << 16) | IgorKey;
+    ITCDLL::ITC_SetSoftKey(DeviceID, key);
+  }
+  catch(const std::exception &e)
+  {
+    CloseDevice(DeviceID);
+    throw e;
+  }
+
   // Set the Device ID in V_value
-  if(int RetVal = SetOperationNumVar(RETURN_VARIABLE, (double) currentDeviceID))
+  if (int RetVal = SetOperationNumVar(RETURN_VARIABLE, (double)currentDeviceID))
   {
     throw IgorException(RetVal);
   }
-
-  // Do ITC_GlobalConfig
-  //
-  // NOTE: Igor will provide the XOP with pointers to individual FIFO.
-  // The internal FIFOs are not used.
-  // Disable the internal FIFOs using ITC_GlobalConfig to save memory
-  ITCGlobalConfig currGlobalConfig;
-  ZeroMemory(&currGlobalConfig, sizeof(currGlobalConfig));
-  currGlobalConfig.SoftwareFIFOSize = 0;
-  ITCDLL::ITC_GlobalConfig(&currGlobalConfig);
-
-  // Initialise the device
-  ITCDLL::ITC_InitDevice(DeviceID, NULL);
-
-  const long key = (InstruKey << 16) | IgorKey;
-  ITCDLL::ITC_SetSoftKey(DeviceID, key);
 
   END_OUTER_CATCH
 }
