4a2cbd200e76f58dbed80de732769cafaf90eb59
Thomas Braun <thomas.braun@byte-physics.de>
2022-05-26 19:41:15 +0200

    src/HelperFunctions: Add GetVersionInfo

--- src/HelperFunctions.cpp
+++ src/HelperFunctions.cpp
@@ -1,6 +1,7 @@
 #include "HelperFunctions.h"
 #include "itcXOP2.h"
 #include "itcdll.h"
+#include "git_version.h"
 #include "fmt/format.h"
 #include <sstream>
 #include <iterator>
@@ -8,6 +9,8 @@
 #include <cmath>
 
 // This file is part of the `ITCXOP2` project and licensed under BSD-3-Clause.
+
+using namespace fmt::literals;
 
 thread_local bool debuggingEnabled = false;
 
@@ -196,4 +199,32 @@ void SetOperationReturn(const std::string &name, double value)
   {
     throw IgorException(err, fmt::format("Error setting {}.", name));
   }
+}
+
+StrStrMap GetVersionInfo(const std::string &xopName)
+{
+  std::map<std::string, std::string> m;
+
+  m["name"]    = xopName;
+  m["version"] = GIT_REVISION;
+
+#ifdef MACIGOR64
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdate-time"
+#endif
+
+  m["builddate"] = "{} {}"_format(__DATE__, __TIME__); // NOLINT
+
+#ifdef MACIGOR64
+#pragma clang diagnostic pop
+#endif
+
+#ifdef MACIGOR64
+  m["compiler"] = "Clang {}.{}.{}"_format(__clang_major__, __clang_minor__,
+                                          __clang_patchlevel__);
+#else
+  m["compiler"] = "Visual Studio {}"_format(_MSC_VER);
+#endif
+
+  return m;
 }
